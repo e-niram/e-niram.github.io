ae529b4a324974e99f474a6e94aa3bb7d38ff5e8
Esteban <emarin.cv@gmail.com>
2024-03-24 13:16:19 -0600

    Add Color Palette

---
+++ src/foundations/Colors.tsx
@@ -0,0 +1,11 @@
+/* Color Palette obtained from:
+ * https://colorhunt.co/palette/2d32504247697077a1f6b17a
+ * https://colorhunt.co/palette/b4b4b8c7c8cce3e1d9f2efe5
+ **/
+export const Colors = {
+	dark: "#2D3250",
+	primary: "#424769",
+	secondary: "#7077A1",
+	light: "#F6B17A",
+	text: "#F2EFE5",
+};
---
+++ src/foundations/Colors.types.ts
@@ -0,0 +1,1 @@
+export type Color = "dark" | "primary" | "secondary" | "light";
